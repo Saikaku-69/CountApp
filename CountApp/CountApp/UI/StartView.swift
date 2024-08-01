d6592fde7aae048125cc19ac446ebd6fc258f21e
Saikaku <grnmnsta@gmail.com>
2024-08-01 22:57:18 +0900

    UI画面の追加

---
+++ CountApp/CountApp/UI/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  CountApp
+//
+//  Created by cmStudent on 2024/08/01.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
